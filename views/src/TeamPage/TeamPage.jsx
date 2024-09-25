d362e6ceb7d9a1fd4ca37bb3b107e77de8a465a5
Zeta54633 <yashmathur.693@gmail.com>
2024-09-25 17:48:48 +0530

    Team page in development

--- views/src/TeamPage/TeamPage.jsx
+++ views/src/TeamPage/TeamPage.jsx
@@ -1,11 +1,16 @@
 
 import Header from "../Components/Header";
+import PlateHolder from "../Components/PlateHolder";
+import PlayerPlate from "../Components/PlayerPlate";
 
 
 function TeamPage()
 {
     return <>
         <Header />
+       
+        <br></br>
+        <PlateHolder/>
     </>
 }
 
